a444c80267c8b5662843f8cd96c38ae1c1e96e53
José de Jesus Filho <jjesusfilho@gmail.com>
2018-12-31 00:40:38 +0000

    corrects read_stf_pdf.

--- R/read_stf_pdf.R
+++ R/read_stf_pdf.R
@@ -1,7 +1,8 @@
 #' Reads pdf files
 #'
-#' @param file of paths to the pdf files to be read.
-#'
+#' @param file Paths to the pdf files to be read.
+#' @param plan Defaults to "sequential". Check \code{future::plan} for more
+#'     information.
 #' @return a tibble with tree columns: incidente, texto, and doc_id.
 #' @export
 #'
@@ -9,20 +10,18 @@
 #' \dontrun{
 #' read_stf_pdf(file="")
 #' }
-read_stf_pdf <- function(file=NULL){
+read_stf_pdf<-function (file = NULL,plan='sequential')
+{
 
-  lista<-vector("list", length(file))
+  future::plan(plan)
 
-  for (i in seq_along(file))  {
-    lista[[i]]<-pdftools::pdf_text(file[i])
-  }
 
-  texto <-  purrr::map_chr(lista,~paste(.x,collapse=""))
+  furrr::future_map_dfr(file, purrr::possibly(~{
 
-    doc_id <- stringr::str_extract(file,"(?<=docid_)\\d+")
-
-    incidente <- stringr::str_extract(file,"(?<=incidente_)\\d+")
-
-    tibble::tibble(incidente=incidente,texto=texto,doc_id=doc_id)
-
+    texto <- pdftools::pdf_text(.x) %>%
+      paste0(collapse="")
+    doc_id <- stringr::str_extract(.x, "(?<=docid_)\\d+")
+    incidente <- stringr::str_extract(.x, "(?<=incidente_)\\d+")
+    tibble::tibble(incidente = incidente, texto = texto, doc_id = doc_id)
+  },NULL),.progress=TRUE)
 }
