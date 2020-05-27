dc06b9195ed34acced792d3878ad55c299aca31e
José de Jesus Filho <jjesusfilho@gmail.com>
2020-05-27 15:18:59 -0300

    "small adjustments"

--- R/read_stf_docket_sheet.R
+++ R/read_stf_docket_sheet.R
@@ -2,33 +2,31 @@
 #'
 #' @param files Vector of html files downloaded by download_docket
 #' @param path Where to find the htmls if files is NULL
-#' @param plan Defalts to sequential. See \code{future::plan} for all options.
+#'
 #' @return tibble with follow-up details
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' andamento <- read_stf_docket_sheet(path = ".", plan = "multiprocess")
+#' andamento <- read_stf_docket_sheet(path = ".")
 #' }
 #'
-read_stf_docket_sheet <- function(files = NULL, path = ".", plan = "sequential") {
+read_stf_docket_sheet <- function(files = NULL, path = ".") {
 
-  oplan <- plan
-
-  on.exit(future::plan(oplan), add = TRUE)
 
 
   if(is.null(files)){
 
-  arquivos <- list.files(path, full.names = TRUE)
+  files <- list.files(path, full.names = TRUE)
 
   }
 
-  incidentes <- stringr::str_extract(arquivos, "\\d+(?=.html)")
 
-  future::plan(plan)
 
-  furrr::future_map2_dfr(arquivos, incidentes, purrr::possibly(~ {
+  purrr::map_dfr(files,  purrr::possibly(purrrogress::withprogress(~{
+
+    incidente <- stringr::str_extract(.x, "\\d+(?=.html)")
+
     item <- xml2::read_html(.x, encoding = "UTF-8") %>%
       xml2::xml_find_all("//div[@class='andamento-item']")
 
@@ -72,9 +70,9 @@ read_stf_docket_sheet <- function(files = NULL, path = ".", plan = "sequential")
 
     doc_id <- stringr::str_extract(doc_url, "\\d{3,}")
 
-    tibble::tibble(incidente = .y, data_andamento, titulo, invalido, orgao_julgador, descricao, doc, doc_url, doc_id) %>%
+    tibble::tibble(incidente = incidente, data_andamento, titulo, invalido, orgao_julgador, descricao, doc, doc_url, doc_id) %>%
       tidyr::unnest() %>%
       dplyr::distinct()
 
-  }, NULL), .progress = TRUE)
+  }), NULL))
 }
