73a23d61860b076c039fc778d4dcb311c14b93c6
José de Jesus Filho <jjesusfilho@gmail.com>
2020-07-14 00:45:19 -0300

    add function stf_extract_date

--- R/stf_extract_date.R
+++ R/stf_extract_date.R
@@ -7,40 +7,29 @@
 #'
 stf_extract_date <- function(x = NULL){
 
+  d <- x %>%
+    stringr::str_sub(-2000) %>%
+    stringr::str_extract_all("Brasília.+?(?=(\\.|\n))") %>%
+    unlist() %>%
+    tail(1) %>%
+    stringr::str_squish() %>%
+    stringr::str_remove("º") %>%
+    lubridate::parse_date_time(orders = "Brasília, %d de %b de %Y",
+                               exact = TRUE, locale = "pt_BR.UTF-8")
 
-  pb <- progress::progress_bar$new(total = length(x))
-
-  purrr::map(x, purrr::possibly(~{
-
-    pb$tick()
-
-    x <- .x %>%
-      stringr::str_sub(-2000)
+  if (length(d) == 0){
 
     d <- x %>%
-      stringr::str_extract_all("Brasília.+?(?=(\\.|\n))") %>%
+      stringr::str_squish() %>%
+      stringr::str_extract_all("\\d{1,2}\\.\\d{1,2}\\.\\d{4}") %>%
       unlist() %>%
       tail(1) %>%
-      stringr::str_squish() %>%
-      stringr::str_remove("º") %>%
-      lubridate::parse_date_time(orders = "Brasília, %d de %b de %Y",
-                                 exact = TRUE, locale = "pt_BR.UTF-8")
+      lubridate::parse_date_time(orders="%d.%m%Y")
+  } else {
 
-    if (length(d) == 0){
+    d <- d
+  }
 
-      d <- x %>%
-        stringr::str_squish() %>%
-        stringr::str_extract_all("\\d{1,2}\\.\\d{1,2}\\.\\d{4}") %>%
-        unlist() %>%
-        tail(1) %>%
-        lubridate::parse_date_time(orders="%d.%m%Y")
-    } else {
-
-      d <- d
-    }
-    d
-  }, NA)) %>%
-    do.call("c",.) %>%
-    as.Date()
+  return(d)
 
 }
