6356f9ef1975403b7dcff1f13b643d4d60e9be13
José de Jesus Filho <jjesusfilho@gmail.com>
2022-12-22 21:06:14 -0300

    Substitui RSQLite por jqr

--- R/stf_download_cjsg.R
+++ R/stf_download_cjsg.R
@@ -15,80 +15,71 @@ stf_download_cjsg <- function(search = "",
 
 
 
-size <- as.integer(size)
+  size <- as.integer(size)
 
-if (size > 200 | is.na(size)){
+  if (size > 200 | is.na(size)){
 
-  stop("Argument size cannot be bigger then 200 \nand must be coercible to numeric")
+    stop("Argument size cannot be bigger then 200 \nand must be coercible to numeric")
 
-}
+  }
 
-base <- base[1]
+  base <- base[1]
 
-### This function uses SQLite json engine to set values to the json body,
-### as there is no R engine to manipulate json as is.
+  ### This function uses SQLite json engine to set values to the json body,
+  ### as there is no R engine to manipulate json as is.
 
-q <- glue::glue_sql(" with cte1 as (
-                    select json_set({modelo},'$.query.function_score.query.bool.filter[0].query_string.query', {search}) as value
-                     ),
-                     cte2 as (
-                      select json_set(value,'$.post_filter.bool.must[0].term.base', {base}) as value from cte1
-                     ),
-                     cte3 as (
-                    select json_set(value,'$.size', {size}) as value from cte2
-                     )
-                     select value from cte3
-                    ", .con = conn)
 
-body <- DBI::dbGetQuery(conn,q) |>
-        dplyr::pull("value")
+  body <- modelo |>
+    jqr::jq(glue::glue('.query.function_score.query.bool.filter[0].query_string.query = "{search}"')) |>
+    jqr::jq(glue::glue('.post_filter.bool.must[0].term.base = "{base}"')) |>
+    jqr::jq(glue::glue('.size = {size}'))
+
 
 
   h1 <- httr::add_headers(`User-Agent` = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:107.0) Gecko/20100101 Firefox/107.0",
-                    `Accept` = "application/json, text/plain, */*",
-                    `Accept-Language` = "en-US,en;q=0.5",
-                    `Accept-Encoding` = "gzip, deflate, br",
-                    `Content-Type` = "application/json",
-                    `Origin` = "https://jurisprudencia.stf.jus.br",
-                    `Connection` = "keep-alive",
-                    `Sec-Fetch-Dest` = "empty",
-                    `Sec-Fetch-Mode` = "cors",
-                    `Sec-Fetch-Site` = "same-origin")
+                          `Accept` = "application/json, text/plain, */*",
+                          `Accept-Language` = "en-US,en;q=0.5",
+                          `Accept-Encoding` = "gzip, deflate, br",
+                          `Content-Type` = "application/json",
+                          `Origin` = "https://jurisprudencia.stf.jus.br",
+                          `Connection` = "keep-alive",
+                          `Sec-Fetch-Dest` = "empty",
+                          `Sec-Fetch-Mode` = "cors",
+                          `Sec-Fetch-Site` = "same-origin")
 
-url <- "https://jurisprudencia.stf.jus.br/api/search/search"
-
-
- conteudo <- httr::POST(url,
-                   encode = "json",
-                   h1,
-                   body = body) |>
-          httr::content("text", encoding = "UTF-8")
+  url <- "https://jurisprudencia.stf.jus.br/api/search/search"
 
 
-  q <- glue::glue_sql("select json_extract({conteudo}, '$.result.hits.total.value') as value", .con = conn)
+  conteudo <- httr::POST(url,
+                         encode = "json",
+                         h1,
+                         body = body) |>
+    httr::content("text", encoding = "UTF-8")
 
 
 
-  total <- DBI::dbGetQuery(conn,q) |>
-             dplyr::pull("value")
+
+
+  total <- conteudo |>
+    jqr::jq(".result.hits.total.value")
+
 
   sequencia <- seq(0, total,by = size )
 
 
   purrr::walk(sequencia, purrr::possibly(~{
 
-  q <- glue::glue_sql("select json_set({body}, '$.from', {.x}) as value", .con = conn)
 
-  body <- DBI::dbGetQuery(conn,q) |>
-          dplyr::pull("value")
+    body <- body |>
+      jqr::jq(glue::glue(".from = {.x}"))
 
-  arquivo <- file.path(dir, paste0("stf_search_page_",.x, ".json") )
+    arquivo <- file.path(dir, paste0("stf_search_page_",.x, ".json") )
 
-  httr::POST(url,
-             encode = "json",
-             h1,
-             body = body,
-             httr::write_disk(arquivo, overwrite = T))
+    httr::POST(url,
+               encode = "json",
+               h1,
+               body = body,
+               httr::write_disk(arquivo, overwrite = T))
 
   },NULL))
 
